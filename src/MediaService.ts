1f6acb3f892c94122a7026c8242155e0de2c1d23
Balazs Kreith <kreith.balazs@gmail.com>
2022-11-07 13:05:53 +0200

    Add MediasoupMonitor from ObserveRTC

--- src/MediaService.ts
+++ src/MediaService.ts
@@ -17,6 +17,8 @@ import { Producer } from 'mediasoup/node/lib/Producer';
 import { DataProducer } from 'mediasoup/node/lib/DataProducer';
 import { DataConsumer } from 'mediasoup/node/lib/DataConsumer';
 import { WebRtcServer } from 'mediasoup/node/lib/WebRtcServer';
+import { MediasoupMonitor, createMediasoupMonitor, MediasoupMonitorConfig, TransportTypeFunction } from "@observertc/sfu-monitor-js";
+import { PlainTransport } from 'mediasoup/node/lib/types';
 
 const logger = new Logger('MediaService');
 
@@ -80,6 +82,7 @@ export default class MediaService {
 	public maxIncomingBitrate: number;
 	public maxOutgoingBitrate: number;
 	public workers = List<Worker>();
+	public readonly monitor: MediasoupMonitor;
 
 	constructor({
 		ip,
@@ -95,6 +98,7 @@ export default class MediaService {
 		this.initialAvailableOutgoingBitrate = initialAvailableOutgoingBitrate;
 		this.maxIncomingBitrate = maxIncomingBitrate;
 		this.maxOutgoingBitrate = maxOutgoingBitrate;
+		this.monitor = this.createMonitor();
 	}
 
 	@skipIfClosed
@@ -351,5 +355,24 @@ export default class MediaService {
 		);
 
 		return this.getOrCreateRouter(roomId, leastLoadedWorkers[0]);
+	}
+
+	@skipIfClosed
+	private createMonitor(): MediasoupMonitor {
+		const getTransportType: TransportTypeFunction = transport => {
+			if (transport instanceof WebRtcTransport) return "webrtc-transport";
+			if (transport instanceof PlainTransport) return "plain-rtp-transport";
+			if (transport instanceof PipeTransport) return "pipe-transport";
+			return "direct-transport";
+		}
+		const config: MediasoupMonitorConfig = {
+			collectingPeriodInMs: 5000,
+			samplingPeriodInMs: 30000,
+			mediasoup,
+			mediasoupCollectors: {
+				getTransportType
+			}
+		}
+		return createMediasoupMonitor(config);
 	}
 }
