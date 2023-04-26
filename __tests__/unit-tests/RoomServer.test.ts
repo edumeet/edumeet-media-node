8ad7cefd0f5222c99a1f2481df148ba203283db1
Håvar Aambø Fosstveit <havar@fosstveit.net>
2023-04-26 08:03:06 +0200

    Remove faulty test

--- __tests__/unit-tests/RoomServer.test.ts
+++ __tests__/unit-tests/RoomServer.test.ts
@@ -4,7 +4,6 @@ import RoomServer, { RoomServerOptions } from '../../src/RoomServer';
 import { RoomServerConnection } from '../../src/RoomServerConnection';
 import MediaServiceMock from '../../__mocks__/MediaServiceMock';
 import RoomServerConnectionMock from '../../__mocks__/RoomServerConnectionMock';
-import RouterMock from '../../__mocks__/RouterMock';
 
 test('Constructor - should call notify on roomServerConnection', () => {
 	const mockMediaService = new MediaServiceMock() as unknown as MediaService;
@@ -19,19 +18,4 @@ test('Constructor - should call notify on roomServerConnection', () => {
 
 	expect(spyNotify).toHaveBeenCalled();
 	expect(sut.closed).toBe(false);
-});
-
-test('should close when RoomServerConnection closes', () => {
-	const mockMediaService = new MediaServiceMock() as unknown as MediaService;
-	const mockConn = new RoomServerConnectionMock() as unknown as RoomServerConnection;
-	const options: RoomServerOptions = {
-		mediaService: mockMediaService,
-		roomServerConnection: mockConn
-	};
-	const sut = new RoomServer(options);
-	const spyClose = jest.spyOn(sut, 'close');
-
-	mockConn.emit('close');
-
-	expect(spyClose).toHaveBeenCalled();
 });
